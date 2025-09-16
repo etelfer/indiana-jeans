99bdea41861e3f5adf44611e4ca7921cf619493b
Erin Telfer <eringtelfer@gmail.com>
2025-09-15 20:37:25 -0500

    created function that displays submissions and changed location function from for loop to .map

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { JeanChoices } from "./JeanChoices.js"
 import { LocationChoices } from "./LocationChoices.js"
 import { SubmissionButton } from "./submissionButton.js"
+import { SubmissionList } from "./submissionsList.js"
 
 const container = document.querySelector("#container")
 
@@ -8,11 +9,13 @@ const render = async () => {
     const jeansHTML = JeanChoices()
     const locationsHTMl = await LocationChoices()
     const buttonHTML = SubmissionButton()
+    const submissionsHTML = await SubmissionList()
 
     container.innerHTML = `
     ${jeansHTML}
     ${locationsHTMl}
     ${buttonHTML}
+    ${submissionsHTML}
     `
 }
 
